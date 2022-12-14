612a2b877b26e2aa53b8915a89a01920faff64ce
Sebastian Jurewicz <senkartan@gmail.com>
2022-12-14 16:31:14 +0100

    Update

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,21 +1,6 @@
 import css from './FriendList.module.css';
 import PropTypes from 'prop-types';
-
-const FriendListItem = ({ avatar, name, isOnline }) => {
-  return (
-    <li className={css.item}>
-      <span className={isOnline ? css.online : css.offline}>{isOnline}</span>
-      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-      <p className={css.name}>{name}</p>
-    </li>
-  );
-};
-
-FriendListItem.propTypes = {
-  avatar: PropTypes.string,
-  name: PropTypes.string,
-  isOnline: PropTypes.bool,
-};
+import FriendListItem from '../FriendListItem/FriendListItem';
 
 const FriendList = ({ friends }) => {
   return (
